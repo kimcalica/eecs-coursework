7e94f448347678051b85f2ddead6f5e034a3eddf
Kim Calica <kimcalica2021@u.northwestern.edu>
2019-05-12 15:22:13 -0500

    Adding my hw5 to the repo

---
+++ hw5/01-unique-messages/js/index.js
@@ -0,0 +1,13 @@
+const names = [
+    "Jane", "Brenda", "Wanda", "Maria", "Jasper",
+    "John", "Malik", "Arjun", "Larry", "Curly", "Moe"
+];
+for (name of names) {
+  document.write("Welcome, " + name + "! Make yourself comfortable.<br>");
+}
+
+// use a for / of loop to loop through each element
+// of the names array and output a message to the screen:
+// document.write("Welcome, " + names[0] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[1] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[2] + "! Make yourself comfortable.<br>");
