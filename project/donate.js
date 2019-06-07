957e4469e02badc920e0edfbbca4c7e9af0d43da
Kim Calica <kimcalica2021@u.northwestern.edu>
2019-06-07 09:07:29 -0500

    adding my project to the repo

--- project/donate.js
+++ project/donate.js
@@ -64,10 +64,10 @@ var days = Math.floor(t / (1000 * 60 * 60 * 24));
 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("day").innerHTML =days ;
-document.getElementById("hour").innerHTML =hours;
+document.getElementById("day").innerHTML = days ;
+document.getElementById("hour").innerHTML = hours;
 document.getElementById("minute").innerHTML = minutes;
-document.getElementById("second").innerHTML =seconds;
+document.getElementById("second").innerHTML = seconds;
 if (t < 0) {
 		clearInterval(x);
 		document.getElementById("timeover").innerHTML = "The Raffle is Now!";
@@ -82,32 +82,32 @@ if (t < 0) {
 const addDonation5 = () => {
   var donation = document.querySelector('.donation').innerHTML;
   var nickel = 0.05
-  var donationnew = Number(donation) + Number(nickel);
-  document.querySelector('.donation').innerHTML = donationnew;
+  var donationnew = parseFloat(donation) + parseFloat(nickel);
+  document.querySelector('.donation').innerHTML = donationnew.toFixed(2);
 };
 document.querySelector('.silver').onclick = addDonation5;
 
 const addDonation1 = () => {
   var donation = document.querySelector('.donation').innerHTML;
   var copper = 0.01;
-  var donationnew = Number(donation) + Number(copper);
-  document.querySelector('.donation').innerHTML = donationnew;
+  var donationnew = parseFloat(donation) + parseFloat(copper);
+  document.querySelector('.donation').innerHTML = donationnew.toFixed(2);
 };
 document.querySelector('.bronze').onclick = addDonation1;
 
 const addDonation10 = () => {
   var donation = document.querySelector('.donation').innerHTML;
   var dime = 0.10;
-  var donationnew = Number(donation) + Number(dime);
-  document.querySelector('.donation').innerHTML = donationnew;
+  var donationnew = parseFloat(donation) + parseFloat(dime);
+  document.querySelector('.donation').innerHTML = donationnew.toFixed(2);
 };
 document.querySelector('.gold').onclick = addDonation10;
 
 const addDonation25 = () => {
   var donation = document.querySelector('.donation').innerHTML;
   var quarter = 0.25;
-  var donationnew = Number(donation) + Number(quarter);
-  document.querySelector('.donation').innerHTML = donationnew;
+  var donationnew = parseFloat(donation) + parseFloat(quarter);
+  document.querySelector('.donation').innerHTML = donationnew.toFixed(2);
 };
 document.querySelector('.gold2').onclick = addDonation25;
 
